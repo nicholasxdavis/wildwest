c2e38a808a611ae1db71f6af54ecfbf1b26b2017
blacnova <147455788+nicholasxdavis@users.noreply.github.com>
2024-06-22 02:42:17 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
-// scripts.js
-
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const navLinks = document.getElementById('nav-links');
 
+    // Toggle menu when menuToggle is clicked
     menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('show');
+    });
+
+    // Close menu when clicking outside of it
+    document.addEventListener('click', (event) => {
+        const isClickInsideMenu = navLinks.contains(event.target);
+        const isClickInsideToggle = menuToggle.contains(event.target);
+        
+        if (!isClickInsideMenu && !isClickInsideToggle) {
+            navLinks.classList.remove('show');
+        }
     });
 });
